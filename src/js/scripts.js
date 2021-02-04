f8f8476c038289bc1633e6e332273759ecb73d45
Michael Burton <micbur1@gmail.com>
2021-02-04 13:50:30 -0800

    add webpack files
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

---
+++ src/js/scripts.js
@@ -0,0 +1,118 @@
+const string = "I don't know why I decided to start coding?"
+
+function createStringArray() {
+  stringArray = []
+  let lowerCaseString = string.toLowerCase();
+  stringArray.push(lowerCaseString.split(''));
+  return stringArray;
+};
+
+let returnedArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+// const loopArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+const keyArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// stringArray = ["i", " ", "d", "o", "n", "'", "t", " ", "k", "n", "o", "w", " ", "w", "h", "y", " ", "i", " ", "d", "e", "c", "i", "d", "e", "d", " ", "t", "o", " ", "s", "t", "a", "r", "t", " ", "c", "o", "d", "i", "n", "g", "?"]
+
+// for(i = 0; i < loopArray.length; i ++){
+//   for(i = 0; i < stringArray.length; i ++){
+//     switch (stringArray[i]) {
+//       case (keyArray[(loopArray[i])]):
+//         (returnedArray[loopArray[i]] += 1);
+//         break;
+//     default:
+//     }
+//   }
+// };
+
+for(i = 0; i < stringArray.length; i ++){
+  switch (stringArray[i]) {
+    case (keyArray[0]):
+      (returnedArray[0] += 1);
+      break;
+    };
+  };
+
+
+  for(i = 0; i < stringArray.length; i ++){
+    if (stringArray[i] = keyArray[0]) {
+        (returnedArray[0] += 1);
+      } 
+      else if (stringArray[i] = keyArray[8]) {
+        (returnedArray[8] += 1);
+      };
+    };
+    // case (keyArray[1]):
+    //   (returnedArray[1].push += 1 );
+    //   break;
+    // case (keyArray[2]):
+    //   (returnedArray[2] += 1);
+    //   break;
+    // case (keyArray[3]):
+    //   (returnedArray[3] += 1 );
+    //   break;
+//     case (keyArray[4]):
+//       (returnedArray[4] += 1);
+//       break;
+//     case (keyArray[5]):
+//       (returnedArray[5] += 1);
+//       break;
+//     case (keyArray[6]):
+//       (returnedArray[6] += 1);
+//       break;
+//     case (keyArray[7]):
+//       (returnedArray[7] += 1);
+//       break;
+//     case (keyArray[8]):
+//       (returnedArray[8] += 1);
+//       break;
+//     case (keyArray[9]):
+//       (returnedArray[9] += 1);
+//       break;
+//     case (keyArray[10]):
+//       (returnedArray[10] += 1);
+//       break;
+//     case (keyArray[12]):
+//       (returnedArray[12] += 1);
+//       break;
+//     case (keyArray[13]):
+//       (returnedArray[13] += 1);
+//       break;
+//     case (keyArray[14]):
+//       (returnedArray[14] += 1);
+//       break;
+//     case (keyArray[15]):
+//       (returnedArray[15] += 1);
+//       break;
+//     case (keyArray[16]):
+//       (returnedArray[16] += 1);
+//       break;
+//     case (keyArray[17]):
+//       (returnedArray[17] += 1);
+//       break;
+//     case (keyArray[18]):
+//       (returnedArray[18] += 1);
+//       break;
+//     case (keyArray[19]):
+//       (returnedArray[19] += 1);
+//       break;
+//     case (keyArray[20]):
+//       (returnedArray[20] += 1);
+//       break;
+//     case (keyArray[21]):
+//       (returnedArray[21] += 1);
+//       break;
+//     case (keyArray[22]):
+//       (returnedArray[22] += 1);
+//       break;
+//     case (keyArray[23]):
+//       (returnedArray[23] += 1);
+//       break;
+//     case (keyArray[24]):
+//       (returnedArray[24] += 1);
+//       break;
+//     case (keyArray[25]):
+//       (returnedArray[25] += 1);
+//       break;
+//     default:
+//   }
+// }
