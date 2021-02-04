2ccbcabe81625ca906b3ec0c9594b00f48686967
Michael Burton <micbur1@gmail.com>
2021-02-04 15:10:30 -0800

    add first passed  test
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- __tests__/test.scripts.js
+++ __tests__/test.scripts.js
@@ -2,16 +2,18 @@ import letterCounter  from '../src/js/scripts2.js';
 // import {createStringArray} from '../src/js/scripts.js'
 
 describe('letterCounter', () =>{
-  beforeEach(function() {
-    
-  });
+  // let returnedArray;
+  // const keyArray;
+  // let stringArray;
+  // beforeEach(() => {
+  // });
   test('Should correctly match stringArray index values with keyArray index values', () => {
-  let returnedArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-  const keyArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  let stringArray = ["i", " ", "d", "o", "n", "'", "t", " ", "k", "n", "o", "w", " ", "w", "h", "y", " ", "i", " ", "d", "e", "c", "i", "d", "e", "d", " ", "t", "o", " ", "s", "t", "a", "r", "t", " ", "c", "o", "d", "i", "n", "g", "?"]
-    expect(letterCounter(stringArray[0])).toEqual(keyArray[8]);
-    });
-});
+    let returnedArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    const keyArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let stringArray = ["i", " ", "d", "o", "n", "'", "t", " ", "k", "n", "o", "w", " ", "w", "h", "y", " ", "i", " ", "d", "e", "c", "i", "d", "e", "d", " ", "t", "o", " ", "s", "t", "a", "r", "t", " ", "c", "o", "d", "i", "n", "g", "?"];
+    expect(letterCounter(stringArray, keyArray, returnedArray)).toEqual(true);
+  });
+})
 
 // test: ("increase the corresponding returnedArray index value by one for each matching letter")
 
--- src/js/scripts2.js
+++ src/js/scripts2.js
@@ -1,3 +1,11 @@
 
-export default function letterCounter() {
-    };
+export default function letterCounter(stringArray, keyArray, returnedArray) {
+  for(let i = 0; i < stringArray.length; i ++){
+    if (stringArray[i] = keyArray[0]) {
+    return true;
+    }
+    else {
+      return false;
+    }
+  };
+};
